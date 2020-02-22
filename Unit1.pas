5557b40ff2e5f8b8a6b9b93d11d44ede09e3e079
gabriel <gabriel.o.s@hotmail.com>
2020-02-21 21:56:34 -0300

    Tela Inicial iFood no delphi

---
+++ Unit1.pas
@@ -0,0 +1,112 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.Objects, FMX.StdCtrls, FMX.Edit, FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    Layout1: TLayout;
+    Layout2: TLayout;
+    Layout3: TLayout;
+    Layout4: TLayout;
+    Layout5: TLayout;
+    Layout6: TLayout;
+    Layout7: TLayout;
+    Layout8: TLayout;
+    Label1: TLabel;
+    Label2: TLabel;
+    Line1: TLine;
+    Line2: TLine;
+    Line3: TLine;
+    Rectangle1: TRectangle;
+    Edit1: TEdit;
+    SpeedButton1: TSpeedButton;
+    Layout9: TLayout;
+    HorzScrollBox1: THorzScrollBox;
+    Layout10: TLayout;
+    Image1: TImage;
+    Label3: TLabel;
+    Layout11: TLayout;
+    Image2: TImage;
+    Label4: TLabel;
+    Layout12: TLayout;
+    Image3: TImage;
+    Label5: TLabel;
+    Layout13: TLayout;
+    Image4: TImage;
+    Label6: TLabel;
+    Layout14: TLayout;
+    Image5: TImage;
+    Label7: TLabel;
+    HorzScrollBox2: THorzScrollBox;
+    Layout15: TLayout;
+    Image6: TImage;
+    Layout16: TLayout;
+    Image7: TImage;
+    Layout17: TLayout;
+    Image8: TImage;
+    Layout18: TLayout;
+    Image9: TImage;
+    Layout19: TLayout;
+    Image10: TImage;
+    Layout20: TLayout;
+    Image11: TImage;
+    Rectangle2: TRectangle;
+    Layout21: TLayout;
+    Label8: TLabel;
+    Label9: TLabel;
+    SpeedButton2: TSpeedButton;
+    Layout22: TLayout;
+    Image12: TImage;
+    Layout23: TLayout;
+    Layout24: TLayout;
+    Label10: TLabel;
+    HorzScrollBox3: THorzScrollBox;
+    Layout26: TLayout;
+    Image13: TImage;
+    Label11: TLabel;
+    Layout27: TLayout;
+    Image14: TImage;
+    Label12: TLabel;
+    Layout28: TLayout;
+    Image15: TImage;
+    Label13: TLabel;
+    Layout29: TLayout;
+    Image16: TImage;
+    Label14: TLabel;
+    Layout30: TLayout;
+    Image17: TImage;
+    Label15: TLabel;
+    Rectangle3: TRectangle;
+    Layout25: TLayout;
+    Layout31: TLayout;
+    Layout32: TLayout;
+    Layout33: TLayout;
+    Image18: TImage;
+    Image19: TImage;
+    Image20: TImage;
+    Image21: TImage;
+    Label16: TLabel;
+    Label17: TLabel;
+    Label18: TLabel;
+    Label19: TLabel;
+    VertScrollBox1: TVertScrollBox;
+    Layout34: TLayout;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
